559b316329853d50f41fc796555127fc3c52504d
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2024-04-13 20:30:59 -0400

    nav tins

--- src/components/themeToggle/ThemeToggle.tsx
+++ src/components/themeToggle/ThemeToggle.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./_theme.module.scss";
+import { FaToggleOff } from "react-icons/fa6";
+import { FaToggleOn } from "react-icons/fa6";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>themetoggle</div>;
+  const [isOn, setIsOn] = useState<Boolean>(false);
+
+  console.log(isOn ? `dark` : `light`);
+  return (
+    <div className={styles.container}>
+      {isOn ? (
+        <FaToggleOn onClick={() => setIsOn(false)} />
+      ) : (
+        <FaToggleOff onClick={() => setIsOn(true)} />
+      )}
+    </div>
+  );
 };
 
 export default ThemeToggle;
